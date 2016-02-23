998140e63f7ee4fdfb5d9c42c4211048dd85db11
Ramesh <hramesh@zeomega.com>
2016-02-23 18:56:15 +0530

    Added the file

---
+++ continue.py
@@ -0,0 +1,20 @@
+for letter in 'Python':   
+   if letter == 'h':
+      continue
+   print 'Current Letter :', letter
+
+var = 10              
+while var > 0:              
+   var = var -1
+   if var == 5:
+      continue
+   print 'Current variable value :', var
+print "Good bye!"
+
+for letter in 'python':
+   if letter == 'h':
+      pass
+      print 'This is pass block'
+   print 'Current Letter :', letter
+
+print "Good bye!"
